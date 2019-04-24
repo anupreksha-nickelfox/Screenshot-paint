2a0ac07f5e512facf552ab2ebbcca582bb4e2683
anupreksha-nickelfox <anupreksha@nickelfox.com>
2019-04-24 14:54:58 +0530

    feat(shake):add shake detection

--- app/src/main/java/com/android/screen/Activity/MainActivity.java
+++ app/src/main/java/com/android/screen/Activity/MainActivity.java
@@ -1,18 +1,23 @@
 package com.android.screen.Activity;
 
+import android.content.Context;
 import android.content.Intent;
 import android.databinding.DataBindingUtil;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
+import android.hardware.Sensor;
+import android.hardware.SensorManager;
 import android.os.Bundle;
 import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.Toast;
 
 import com.android.screen.EditImageActivity;
 import com.android.screen.R;
 import com.android.screen.databinding.ActivityMainBinding;
 import com.android.screen.utils.ScreenshotManager;
+import com.android.screen.utils.ShakeDetector;
 
 import java.io.ByteArrayOutputStream;
 
@@ -20,6 +25,9 @@ import static com.android.screen.utils.Constant.SCREENSHOT_BITMAP;
 
 public class MainActivity extends AppCompatActivity {
 
+    private SensorManager mSensorManager;
+    private Sensor mAccelerometer;
+    private ShakeDetector mShakeDetector;
     private View main;
     ActivityMainBinding binding;
     Bitmap b;
@@ -29,6 +37,32 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
         main = getWindow().getDecorView().getRootView();
+        mSensorManager = (SensorManager) getSystemService(Context.SENSOR_SERVICE);
+        mAccelerometer = mSensorManager
+                .getDefaultSensor(Sensor.TYPE_ACCELEROMETER);
+        mShakeDetector = new ShakeDetector();
+        mShakeDetector.setOnShakeListener(new ShakeDetector.OnShakeListener() {
+
+            @Override
+            public void onShake(int count) {
+                /*
+                 * The following method, "handleShakeEvent(count):" is a stub //
+                 * method you would use to setup whatever you want done once the
+                 * device has been shook.
+                 */
+                //tvShake.setText("Shake Action is just detected!!");
+                Toast.makeText(MainActivity.this, "Shaked!!!", Toast.LENGTH_SHORT).show();
+                b = ScreenshotManager.screenShot(main);
+                binding.imageView.setImageBitmap(b);
+                Intent intent = new Intent(MainActivity.this, EditImageActivity.class);
+                ByteArrayOutputStream stream = new ByteArrayOutputStream();
+                b.compress(Bitmap.CompressFormat.PNG, 50, stream);
+                byte[] bytes = stream.toByteArray();
+                intent.putExtra(SCREENSHOT_BITMAP,bytes);
+                startActivity(intent);
+                finish();
+            }
+        });
     }
 
     public void CaptureScreen(View v){
@@ -41,6 +75,20 @@ public class MainActivity extends AppCompatActivity {
                 intent.putExtra(SCREENSHOT_BITMAP,bytes);
                 startActivity(intent);
                 finish();
+    }
+
+    @Override
+    public void onResume() {
+        super.onResume();
+        // Add the following line to register the Session Manager Listener onResume
+        mSensorManager.registerListener(mShakeDetector, mAccelerometer,	SensorManager.SENSOR_DELAY_UI);
+    }
+
+    @Override
+    public void onPause() {
+        // Add the following line to unregister the Sensor Manager onPause
+        mSensorManager.unregisterListener(mShakeDetector);
+        super.onPause();
     }
 
 }
